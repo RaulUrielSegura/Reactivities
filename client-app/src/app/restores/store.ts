b4696d4b3aef1bbc01802202431e020db393fb74
raelsh <raul.segura@softtek.com>
2022-05-26 03:35:16 -0500

    End of section 8

--- client-app/src/app/restores/store.ts
+++ client-app/src/app/restores/store.ts
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import ActivityStore from "./activityStore.ts";
+import ActivityStore from "./activityStore";
 
 interface Store{
     activityStore: ActivityStore
