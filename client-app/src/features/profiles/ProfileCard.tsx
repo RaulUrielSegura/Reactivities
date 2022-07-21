6eb3cdc330cd9c2d52fff78f6e4f973a6f53ce5b
raelsh <raul.segura@softtek.com>
2022-07-20 22:51:52 -0500

    End of section 20

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Card, Icon, Image } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
+import FollowButton from "./FollowButton";
 
 interface Props{
     profile:Profile;
@@ -11,15 +12,16 @@ interface Props{
 export default observer(function ProfileCard({profile}:Props){
     return(
         <Card as={Link} to={`/profiles/${profile.username}`}>
-            <Image src={profile.image || 'assets/user.png'}/>
+            <Image src={profile.image || '/assets/user.png'}/>
             <Card.Content>
                 <Card.Header>{profile.displayName}</Card.Header>
                 <Card.Description>Bio</Card.Description>
             </Card.Content>
             <Card.Content extra>
                 <Icon name="user"/>
-                20 followers
+                {profile.followersCount} followers
             </Card.Content>
+            <FollowButton profile={profile} />
         </Card>
     )
 })
