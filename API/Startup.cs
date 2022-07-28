cfe831110ac6341a1311cdcd6e8c67cea47bea18
raelsh <raul.segura@softtek.com>
2022-07-28 05:40:05 -0500

    Security Headers added v2

--- API/Startup.cs
+++ API/Startup.cs
@@ -59,7 +59,7 @@ namespace API
             app.UseReferrerPolicy(opt=>opt.NoReferrer());
             app.UseXXssProtection(opt=>opt.EnabledWithBlockMode());
             app.UseXfo(opt=>opt.Deny());
-            app.UseCspReportOnly(opt=>opt
+            app.UseCsp(opt=>opt
                 .BlockAllMixedContent()
                 .StyleSources(s=>s.Self().CustomSources("https://fonts.googleapis.com","https://cdnjs.cloudflare.com"))
                 .FontSources(s=>s.Self().CustomSources("https://fonts.gstatic.com", "https://cdnjs.cloudflare.com","data:"))
@@ -74,7 +74,7 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }else{
-                app.Use(async(context, next)=>{
+                app.Use(async(context, next)=>{                   
                     context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
                     await next.Invoke();
                 });
